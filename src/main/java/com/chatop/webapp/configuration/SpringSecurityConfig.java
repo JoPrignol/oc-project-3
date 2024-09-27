3ccc1db899436161f8ac3716026d091585046c59
Joanny Prignol <joannyprignol@gmail.com>
2024-09-27 17:57:51 +0200

    mise en place de la chaine de filtres et création d'utilisateurs test
    Signed-off-by: Joanny Prignol <joannyprignol@gmail.com>

--- src/main/java/com/chatop/webapp/configuration/SpringSecurityConfig.java
+++ src/main/java/com/chatop/webapp/configuration/SpringSecurityConfig.java
@@ -1,16 +1,53 @@
 package com.chatop.webapp.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
+	// Mise en place de filtres en fonction des rôles des utilisateurs. 
+	@Bean
 	public SecurityFilterChain FilterChain(HttpSecurity http) throws Exception {
-		return http.build();
+		return http.authorizeHttpRequests(auth -> {
+			auth.requestMatchers("/admin").hasRole("ADMIN");
+			auth.requestMatchers("/user").hasRole("USER");
+			// Faire en sorte que toutes les requêtes soient sécurisées
+			auth.anyRequest().authenticated();
+		}).formLogin(Customizer.withDefaults()).build();
+	}
+	
+	@Bean
+	public UserDetailsService users() {
+		UserDetails user = User.builder()
+				.username("user")
+				.password(passwordEncoder().encode("user"))
+				.roles("USER")
+				.build();
+		UserDetails admin = User.builder()
+				.username("admin")
+				.password(passwordEncoder().encode("admin"))
+				.roles("ADMIN", "USER")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
+
+
