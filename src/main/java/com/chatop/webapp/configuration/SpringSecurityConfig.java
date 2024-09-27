23269b5d7e40c5560b4449ce98c4372667ef19bf
Joanny Prignol <joannyprignol@gmail.com>
2024-09-27 16:59:04 +0200

    spring security default configuration
    Signed-off-by: Joanny Prignol <joannyprignol@gmail.com>

---
+++ src/main/java/com/chatop/webapp/configuration/SpringSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.chatop.webapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+	public SecurityFilterChain FilterChain(HttpSecurity http) throws Exception {
+		return http.build();
+	}
+	
+}
