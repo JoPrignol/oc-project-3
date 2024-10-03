320d59429833edda9dbdbde7d98179058eb9a2ae
Joanny Prignol <joannyprignol@gmail.com>
2024-10-03 12:26:46 +0200

    création du login controller et lien vers la DB
    Signed-off-by: Joanny Prignol <joannyprignol@gmail.com>

--- src/main/java/com/chatop/webapp/ChatopApplication.java
+++ src/main/java/com/chatop/webapp/ChatopApplication.java
@@ -3,10 +3,16 @@ package com.chatop.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ChatopApplication {
 
 	public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.configure().load();
+    System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(ChatopApplication.class, args);
 	}
 
