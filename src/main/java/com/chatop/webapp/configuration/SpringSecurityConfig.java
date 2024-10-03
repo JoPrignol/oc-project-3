af0154e29abb5415316707b0806e9a3ae93313f0
Joanny Prignol <joannyprignol@gmail.com>
2024-10-03 18:36:07 +0200

    configuration de spring security
    Signed-off-by: Joanny Prignol <joannyprignol@gmail.com>

--- src/main/java/com/chatop/webapp/configuration/SpringSecurityConfig.java
+++ src/main/java/com/chatop/webapp/configuration/SpringSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.chatop.webapp.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,4 +30,13 @@ public class SpringSecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 
+  @Autowired
+  private CustomUserDetailsService customUserDetailsService;
+
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+	  AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+    authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	  return authenticationManagerBuilder.build();
+  }
 }
